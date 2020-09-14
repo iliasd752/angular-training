a067980f06a0def80e7fb54fe5a8279588d7e752
Ilias Dinia <iliasd752@gmail.com>
2020-09-15 00:44:08 +0200

    Table Component #25607

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('first-app app is running!');
+    expect(compiled.querySelector('span').textContent).toContain('first-app app is running!');
   });
 });
